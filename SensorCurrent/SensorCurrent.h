d97c22785d4a2f7f67a0b43399f4d4175937fc20
llbernat <llbernat@gmail.com>
2021-05-12 17:24:27 +0200

    errorSignal revamp

--- SensorCurrent/SensorCurrent.h
+++ SensorCurrent/SensorCurrent.h
@@ -36,26 +36,3 @@
 
 #define WIFI_SSID      "CASBASCOS3TPL" 
 #define WIFI_PASS      "miradordemontepinar3tpl" 
-
-//
-// The public client cert (issuer, subject, cn and pub key)
-//
-#define MY_CERT        \
-"-----BEGIN CERTIFICATE-----\n" \
-"MIICATCCAacCFEbk1rl5gO2xhz9X1JhEEq7t5JJ2MAoGCCqGSM49BAMCMIGVMQsw\n" \
-"CQYDVQQGEwJFUzEWMBQGA1UECAwNSWxsZXMgQmFsZWFyczEPMA0GA1UEBwwGU29s\n" \
-"bGVyMQwwCgYDVQQKDANVSUIxDTALBgNVBAsMBE1VU0kxEDAOBgNVBAMMB211c2kt\n" \
-"Y2ExLjAsBgkqhkiG9w0BCQEWH2xsdWlzLmJlcm5hdDJAZXN0dWRpYW50LnVpYi5j\n" \
-"YXQwHhcNMjEwNTExMTczMjIxWhcNMjIwNTExMTczMjIxWjBwMQswCQYDVQQGEwJF\n" \
-"UzEWMBQGA1UECBMNSWxsZXMgQmFsZWFyczEPMA0GA1UEBxMGU29sbGVyMQwwCgYD\n" \
-"VQQKEwNVSUIxDTALBgNVBAsTBE1VU0kxGzAZBgNVBAMTEjAxMjNDMTNEQTI4M0FG\n" \
-"QTVFRTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABBKBZt1IcdTSotiioiDnckLh\n" \
-"uumZ3MZIDmudxxKeI1ZYMoG6d7d8emyj4ZQgm3wCVUw6laRRZN/y6EwLel0jz28w\n" \
-"CgYIKoZIzj0EAwIDSAAwRQIgBSBTVvPUf+BEPQKVFsT3Ol2hQ2lM+Oh4j0An2MFR\n" \
-"vRcCIQD+lF5hk5JHWlI8sbx9xzeuzM44PBUUXHufKpYyvDrSBA==\n" \
-"-----END CERTIFICATE-----\n"
-
-//
-// ECC608 slot with the corresponding private key 
-//
-#define MY_KEY_SLOT    0
--- SensorCurrent/SensorCurrent.ino
+++ SensorCurrent/SensorCurrent.ino
@@ -28,8 +28,6 @@
 
  */
 
-
-
 /*
  
   Needed third parties libs
@@ -44,9 +42,9 @@
   
  */
 
-// We need WiFi 
+// We need WiFi
 #include <WiFiNINA.h>
-// Also MQTT 
+// Also MQTT
 #include <PubSubClient.h>
 // We want client and server (mutual) autentication
 #include <ArduinoBearSSL.h>
@@ -59,22 +57,19 @@
 //
 //#define OTA_UPDATES
 //
-#ifdef OTA_UPDATES 
+#ifdef OTA_UPDATES
 #include <ArduinoOTA.h>
 #endif
 
-
-// The server Certificate Authority 
-// This file has been (re)generated 
+// The server Certificate Authority
+// This file has been (re)generated
 // using pycert_bearssl tool from CA cert
-#include "certs/musi-ca.h" 
+#include "certs/musi-ca.h"
 
-// Our public cert and 
-// AtmelECC608 slot number that 
-// has the private (and secret) key 
+// Our public cert and
+// AtmelECC608 slot number that
+// has the private (and secret) key
 #include "certs/sensor-cert.h"
-
-
 
 /*
  
@@ -83,26 +78,50 @@
  */
 #include "SensorCurrent.h"
 
-
-
 /* 
 
   Global types
   
  */
 
-// The error codes 
-typedef enum {
-  ERR_OK, 
-  ERR_NO_ECCX, 
+//
+// The error codes
+//
+#define MAX_SIGTXTLEN 7 // max message error 'morse' length
+#define LED_FLASH 100   // time slice unit (ms)
+#define LED_DOT 2       // slice units for dot
+#define LED_DASH 5      // slice units for dash
+//
+typedef enum
+{
+  ERR_OK,
+  ERR_NO_ECCX,
   ERR_ECCX_UNLOCKED,
-  ERR_WIFI_HW, 
-  ERR_WIFI_CREDENTIALS, 
+  ERR_WIFI_HW,
+  ERR_WIFI_CREDENTIALS,
   ERR_WIFI_GET_TIME,
   ERR_MQTT
-  } errorCode;
+} errCode;
+//
+// The error cadency table
+//
+struct morseError
+{
+  errCode code;
+  char message[MAX_SIGTXTLEN + 1];
+  uint8_t lastDelay; // number of FLASH units minus 1, to delay at last
+  bool forever;      // halt processing and signal error forever ?
+};
 
-
+struct morseError errTab[] = {
+    {ERR_OK,               "-.-....", 9, false}, // once
+    {ERR_NO_ECCX,          "-.-..-.", 19, true}, // forever (halt program)
+    {ERR_ECCX_UNLOCKED,    "-.-..--", 19, true}, // forever
+    {ERR_WIFI_HW,          "-.-.-..", 19, true}, // forever
+    {ERR_WIFI_CREDENTIALS, "-.-.-.-", 9, false}, // once
+    {ERR_WIFI_GET_TIME,    "-.-.--.", 9, false}, // once
+    {ERR_MQTT,             "-.-..--", 9, false}  // once
+};
 
 /*
   
@@ -111,41 +130,24 @@ typedef enum {
  */
 
 //
-// Client certificate and key 
-// saved at secrets/SensorSecrets.h
+// MQTT parameters (defined in .h)
 //
-//const char  my_cert[]    = MY_CERT;
-//const char  my_key[]     = MY_KEY;
-
-const char* mqttServer   = MQTT_SERVER;
-uint        mqttPort     = MQTT_PORT;
-
-char        mqttTopic[]  = MQTT_TOPIC;
-
+const char *mqttServer = MQTT_SERVER;
+uint mqttPort = MQTT_PORT;
+//
+char mqttTopic[] = MQTT_TOPIC;
+//
 // Metering period
-long periodMetering = PERIOD; 
-
-// Last milis() we have made a measurement 
+//
+long periodMetering = PERIOD;
+//
+// Last milis() we have made a measurement
+//
 long lastInstant = 0;
-
-
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-
-
 //
 // Instantiate de WiFi Client
 //
-WiFiClient   wifiClient;
+WiFiClient wifiClient;
 //
 // And encapsulate it into an SSLClient
 //
@@ -153,9 +155,7 @@ BearSSLClient wifiClientSSL(wifiClient, TAs, (size_t)TAs_NUM);
 //
 // Also instantiate MQTT client (publisher)
 //
-PubSubClient client(mqttServer, mqttPort, callback, wifiClientSSL);
-
-
+PubSubClient client(mqttServer, mqttPort, wifiClientSSL); // No callback, publisher role only
 
 /*
 
@@ -163,51 +163,78 @@ PubSubClient client(mqttServer, mqttPort, callback, wifiClientSSL);
   
  */
 
-
-void signal_error(errorCode what_error, bool forever = false)
+void signal_error(errCode whatError)
 {
   //
   // Let the LED flash a little
   // to signal to the user the type of error
   //
-  do {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) 
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW 
-    delay(1000);
-  } while (forever); // if fatal error, signal forever
+  const uint n = sizeof(errTab) / sizeof(morseError);
+  bool forever;
+  uint i = 0;
+  uint8_t j;
+
+  while (errTab[i].code != whatError and i < (n - 1))
+    i++;
+
+  if (errTab[i].code == whatError)
+  {
+    do
+    {
+      j = 0;
+      while ((j < MAX_SIGTXTLEN) and errTab[i].message[j] != 0)
+      {
+        switch (errTab[i].message[j]) 
+        {
+          case '-': // dash
+            digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+            delay(LED_DASH * LED_FLASH);
+            digitalWrite(LED_BUILTIN, LOW); // turn the LED off
+            delay(LED_FLASH);
+            break;
+          case '.': //dot
+            digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+            delay(LED_DOT * LED_FLASH);
+            digitalWrite(LED_BUILTIN, LOW); // turn the LED off
+            delay(LED_FLASH);
+            break;
+        }
+        j++;
+      }
+      delay(errTab[i].lastDelay * LED_FLASH);
+    } while (errTab[i].forever); // if fatal error, signal forever
+  }
 }
 
-
-bool reconnect() 
+bool reconnect()
 {
   bool isConnected;
-  
+
   Serial.print("Attempting MQTT connection...");
   // Create a random client ID
   String clientId = "ArduinoClient-";
   clientId += String(ECCX08.random(0xffff), HEX);
   // Attempt to connect
   isConnected = client.connect(clientId.c_str());
-  if (isConnected) 
+  if (isConnected)
   {
     Serial.println("connected");
-  } else 
+  }
+  else
   {
     Serial.print("failed, rc=");
     Serial.print(client.state());
     Serial.print(" / SSL Client error code=");
     Serial.println(wifiClientSSL.errorCode());
     Serial.println("Will try again in a few seconds");
-    signal_error(ERR_MQTT); // signal error and return 
+    signal_error(ERR_MQTT); // signal error and return
     delay(3000);
   }
 
   return isConnected;
 }
 
-
-void setup_wifi() 
+void setup_wifi()
 {
   //
   // Take care of WiFi setup
@@ -215,32 +242,33 @@ void setup_wifi()
 
   // Create and initialize local needed vars
   //
-  char ssid[] = WIFI_SSID;   // in secrets file ! 
-  char pass[] = WIFI_PASS;   // the same !
-  int status = WL_IDLE_STATUS; 
-  
-  delay(100); // wait a little bit to assure readiness 
-  
+  char ssid[] = WIFI_SSID; // in secrets file !
+  char pass[] = WIFI_PASS; // the same !
+  int status = WL_IDLE_STATUS;
+
+  delay(1000); // wait a little bit to assure readiness
+
   // check for the presence of the shield/hardware
   //
-  if (WiFi.status() == WL_NO_SHIELD) {
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
     Serial.println("WiFi shield not present or card w/o WiFi");
-    signal_error(ERR_WIFI_HW, true); // signal a permanent error 
+    signal_error(ERR_WIFI_HW); // signal a permanent error
   }
 
-  // Try to connect 
+  // Try to connect
   //
-  while (status != WL_CONNECTED) {
+  while (status != WL_CONNECTED)
+  {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network.
     status = WiFi.begin(ssid, pass);
   }
-  
 }
 
-
-void printWifiStatus() {
+void printWifiStatus()
+{
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -257,60 +285,62 @@ void printWifiStatus() {
   Serial.println(" dBm");
 }
 
-
-void printResolvedName(const char * hostname)
+void printResolvedName(const char *hostname)
 {
   IPAddress result;
-  int err = WiFi.hostByName(hostname, result) ;
+  int err = WiFi.hostByName(hostname, result);
 
   Serial.print("Resolving: ");
   Serial.println(hostname);
-  if(err == 1){
-        Serial.print("  The IP address is: ");
-        Serial.println(result);
-  } else {
-        Serial.print("  Error code: ");
-        Serial.println(err);
+  if (err == 1)
+  {
+    Serial.print("  The IP address is: ");
+    Serial.println(result);
+  }
+  else
+  {
+    Serial.print("  Error code: ");
+    Serial.println(err);
   }
 }
-
 
 void setup_mTLS()
 {
   //
-  // Tell BearSSL where is the private key 
+  // Tell BearSSL where is the private key
   // and give it the public cert signed by
   // our CA
   //
   wifiClientSSL.setEccSlot(MY_KEY_SLOT, MY_CERT);
 }
 
-
-unsigned long getTime() {
+unsigned long getTime()
+{
   //
-  // Get current time callback 
+  // Get current time callback
   //
   // will be called from ArduinoBearSSLClass::getTime()
   //
   return WiFi.getTime();
 }
 
-
-void setup_eccx08() {
+void setup_eccx08()
+{
   //
   // Start comm with ECCX08 chip
   //
-  if (!ECCX08.begin()) {
+  if (!ECCX08.begin())
+  {
     Serial.println("Failed to communicate with ECC508/ECC608!");
-    signal_error(ERR_NO_ECCX, true);
+    signal_error(ERR_NO_ECCX);
   }
 
-  if (!ECCX08.locked()) {
+  if (!ECCX08.locked())
+  {
     Serial.println("The ECC508/ECC608 is not locked!");
-    signal_error(ERR_ECCX_UNLOCKED, true);
+    signal_error(ERR_ECCX_UNLOCKED);
   }
 }
-
 
 /*
 
@@ -318,10 +348,10 @@ void setup_eccx08() {
  
  */
 
- 
-void setup() {
+void setup()
+{
   //
-  // We will use de builtin led to signal errors or progress 
+  // We will use de builtin led to signal errors or progress
   //
   pinMode(LED_BUILTIN, OUTPUT);
   //
@@ -335,9 +365,9 @@ void setup() {
   setup_eccx08();
   //
   // WiFi init
-  // 
+  //
   setup_wifi();
-  //  
+  //
   // Show wifi config
   //
   printWifiStatus();
@@ -346,20 +376,21 @@ void setup() {
   //
   printResolvedName(mqttServer);
 
-  //
-  // Allow OTA updates
-  //
-  // start the WiFi OTA library with internal (flash) based storage
-  //
-  #ifdef OTA_UPDATES
+//
+// Allow OTA updates
+//
+// start the WiFi OTA library with internal (flash) based storage
+//
+#ifdef OTA_UPDATES
   ArduinoOTA.begin(WiFi.localIP(), "Arduino", OTA_PASS, InternalStorage);
-  #endif
-  
+#endif
+
   //
   // Get current time for correct certs validation
   //
   Serial.print("Testing get current time function ");
-  while (getTime() == 0) {
+  while (getTime() == 0)
+  {
     Serial.print(".");
     signal_error(ERR_WIFI_GET_TIME);
   }
@@ -373,65 +404,62 @@ void setup() {
   setup_mTLS();
 }
 
-
-
 /*
 
  Main loop
  
  */
 
- 
-void loop() {
+void loop()
+{
   //
   // local working vars
   //
-  bool isConnected; 
+  bool isConnected = false;
   //
   char payLoad[MAX_PAYLEN];
-  
-  //
-  // check for WiFi OTA updates
-  //
-  #ifdef OTA_UPDATES 
+
+//
+// check for WiFi OTA updates
+//
+#ifdef OTA_UPDATES
   ArduinoOTA.poll();
-  #endif
+#endif
 
   //
-  // Verify we have connection
-  // and otherwise, reconnect
+  // Verify and mantain MQTT connection,
+  // otherwise reconnect
   //
-  if (!client.connected()) 
-  {
+  if (!client.connected())
     isConnected = reconnect();
-  }
+  else
+    client.loop(); // mantain MQTT connection
 
   long now = millis();
   //
-  // if its metering time and have connection, 
+  // if its metering time and have connection,
   // then sample and send
   //
-  if ((now - lastInstant > periodMetering) and isConnected)
+  if ((now - lastInstant >= periodMetering) and isConnected)
   {
     lastInstant = now;
 
-    Serial.print("Sampling ... "); 
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    Serial.print("Sampling ... ");
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
 
     String lecture1 = String(ECCX08.random(1000));
-    
+
     lecture1.toCharArray(payLoad, MAX_PAYLEN);
-    
+
     Serial.print("publishing ... ");
     client.publish(mqttTopic, payLoad);
     //
-    // force encryption and transmission, 
+    // force encryption and transmission,
     // cause SSL lib optimization
     //
     client.flush();
-    
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+
+    digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
     Serial.println("published");
   }
-  
 }
